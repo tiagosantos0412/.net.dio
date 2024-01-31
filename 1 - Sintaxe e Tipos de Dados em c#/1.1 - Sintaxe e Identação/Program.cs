c123c48b05290b8d8f313e94c2687c211431a8fa
Tiago <tiagosantos0412@gmail.com>
2024-01-31 17:20:52 -0300

    Introdução ao C#, Sintaxe, operadores e conversão.

---
+++ 1 - Sintaxe e Tipos de Dados em c#/1.1 - Sintaxe e Identação/Program.cs
@@ -0,0 +1,34 @@
+﻿using _1._1___Sintaxe_e_Identação.models;
+
+// Declarando variáveis
+// Inteiros:
+int numeroInteiro = 10;
+
+// Ponto Flutuante:
+float numeroFlutuante = 3.14f;
+double numeroDuplo = 325.14;
+
+// Caracteres:
+char caractere = 'A';
+
+// Booleano:
+bool verdadeiroOuFalso = true;
+
+// Tipos de Dados Compostos:
+// Cadeia de Caracteres (String):
+string texto = "Hello, World!";
+
+// Declarando uma variável como Date Time
+DateTime dataAtual = DateTime.Now;
+
+//Instanciando uma classe e atribuindo e exibindo os valores dos atributos
+Pessoa pessoa1 = new Pessoa();
+
+pessoa1.Nome = "Tiago";
+pessoa1.Idade = 37;
+
+pessoa1.Apresentar();
+
+Console.WriteLine("Data e hora: " + dataAtual);
+Console.WriteLine("Data e hora: " + dataAtual.ToString("dd/mm/yyyy"));
+ 
---
+++ 1 - Sintaxe e Tipos de Dados em c#/1.2 - Tipos de Operadores/Program.cs
@@ -0,0 +1,50 @@
+﻿// Introdução ao operador de atribuição
+// int a = 10;
+// int b = 30;
+// int c = a + b;
+
+// Console.WriteLine($"Valor de C =  {c}");
+
+//Combinando Operadores
+//c = c + 5;
+// c += 5;
+// c -= 10;
+// Console.WriteLine($"Valor de C =  {c}");
+
+//Convertendo Tipos de Variáveis (Cast ou Casting)
+// int a = 5;
+// a = Convert.ToInt32("25");
+// int b = int.Parse("52");
+// int c = 21;
+// c = 15;
+// Console.WriteLine($"Valor de a =  {a}");
+// Console.WriteLine($"Valor de b =  {b}");
+// Console.WriteLine($"Valor de c =  {c}");
+
+//Conversão para String
+// int inteiro = 5;
+// string a = Convert.ToString(inteiro);
+// Console.WriteLine($"Valor de a = {a} {a.GetType()}");
+
+//Conversão Implícita
+// int a = 5;
+// Console.WriteLine($"Valor de a = {a} {a.GetType()}");
+// double b = a;
+// Console.WriteLine($"Valor de a = {b} {b.GetType()}");
+// int c = int.MaxValue;
+// Console.WriteLine($"Valor de a = {c} {c.GetType()}");
+// long d = c;
+// Console.WriteLine($"Valor de a = {d} {d.GetType()}");
+
+//Ordem dos operadores o C# segue a mesma regra da hierarquia dos operadores matemáticos
+// double a  = 4 / (2 + 2);
+
+// Console.WriteLine(a);
+
+//Convertendo com segurança
+string a = "15-";
+int b = 0;
+int.TryParse(a, out b);
+Console.WriteLine(b);
+Console.WriteLine("Conversão realizada com sucesso!");
+
